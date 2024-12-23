4d29a3a5ad4ae7d5fa655113107b62981659d6af
AGKolosov <ak60ru@gmail.com>
2024-12-23 23:23:11 +0300

    The homework is done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,84 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Task 1");
+        int capital = 2_459_000;
+        int sum = 0;
+        int salary = 15_000;
+        double percent = 1D / 100;
+        int month = 0;
+        while (sum < capital) {
+            sum += salary;
+            sum = (int) (sum * (1 + percent));
+            month++;
+            System.out.println("Месяц " + month + ", сумма накоплений равна " + sum + " рублей");
+        }
+        System.out.println("Task 2");
+        int number = 0;
+        while (number < 10) {
+            number++;
+            System.out.print(number + " ");
+        }
+        System.out.println();
+        for (; number >= 1; number--) {
+            System.out.print(number + " ");
+        }
+        System.out.println();
+        System.out.println("Task 3");
+        int population = 12_000_000;
+        int fertilityPerThousand = 17;
+        int mortalityPerThousand = 8;
+        int currentYear = 2024;
+        for (int year = currentYear; year < currentYear + 10; year++) {
+            population += population * fertilityPerThousand / 1000 - population * mortalityPerThousand / 1000;
+            System.out.println("Год " + year + ", численность населения составляет " + population);
+        }
+        System.out.println();
+        System.out.println("Task 4");
+        capital = 12_000_000;
+        sum = salary;
+        percent = 7D / 100;
+        month = 0;
+        while (sum < capital) {
+            sum = (int) (sum * (1 + percent));
+            month++;
+            System.out.println("Месяц " + month + ", сумма накоплений равна " + sum + " рублей");
+        }
+        System.out.println();
+        System.out.println("Task 5");
+        sum = salary;
+        month = 0;
+        while (sum < capital) {
+            sum = (int) (sum * (1 + percent));
+            month++;
+            if (month % 6 == 0) {
+                System.out.println("Месяц " + month + ", сумма накоплений равна " + sum + " рублей");
+            }
+        }
+        System.out.println("Task 6");
+        sum = salary;
+        month = 0;
+        int months = 12 * 9;
+        while (month < months) {
+            sum = (int) (sum * (1 + percent));
+            month++;
+            if (month % 6 == 0) {
+                System.out.println("Месяц " + month + ", сумма накоплений равна " + sum + " рублей");
+            }
+        }
+        System.out.println("Task 7");
+        int firstFriday = 6;
+        for (int day = firstFriday; day <= 31; day += 7) {
+            System.out.println("Сегодня пятница, " + day + "-е число. Необходимо подготовить отчет");
+        }
+        System.out.println("Task 8");
+        int period = 79;
+        int startSeeing = 0;
+        int start = currentYear - 200;
+        int end = currentYear + 100;
+        for (int year = startSeeing; year < end; year += period) {
+            if (year > start) {
+                System.out.println(year);
+            }
+        }
     }
 }
